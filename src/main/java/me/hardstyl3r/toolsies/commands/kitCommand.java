5903bf0f05c5547d2068912e12bc7e1268430dce
Aleksander Gencel <johnnylittleplanet@gmail.com>
2021-01-12 16:30:25 +0100

    commands: kitCommand: Support gifting kits
     * Gifting kits require both sender and target to
       have correct permissions.
       sender needs either
         "toolsies.kits.others.<kit>" or
         "toolsies.kits.others.bypass"
       and the target needs
         "toolsies.kits.<kit>".
       Or at least that the idea.

--- src/main/java/me/hardstyl3r/toolsies/commands/kitCommand.java
+++ src/main/java/me/hardstyl3r/toolsies/commands/kitCommand.java
@@ -4,6 +4,7 @@ import me.hardstyl3r.toolsies.Toolsies;
 import me.hardstyl3r.toolsies.managers.KitManager;
 import me.hardstyl3r.toolsies.managers.UserManager;
 import me.hardstyl3r.toolsies.objects.User;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -46,15 +47,49 @@ public class kitCommand implements CommandExecutor {
             kitManager.giveKit(p, kit);
             sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
                     u.getLocale().getConfig().getString("kit.kit_applied")).replace("<name>", kit));
-        } else {
-            Set<String> all = kitManager.getKits(sender);
-            if (all.isEmpty()) {
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
-                        u.getLocale().getConfig().getString("kit.no_kits_available")));
-            } else {
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
-                        u.getLocale().getConfig().getString("kit.available_kits")).replace("<kits>", all.toString()));
+            return true;
+        } else if(args.length == 2){
+            String kit = args[0];
+            if(sender.hasPermission("toolsies.kits.others")) {
+                if (!kitManager.isKit(kit) || !sender.hasPermission("toolsies.kits." + kit.toLowerCase())) {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                            u.getLocale().getConfig().getString("kit.unknown_kit")).replace("<name>", kit));
+                    return true;
+                }
+                Player target = Bukkit.getPlayerExact(args[1]);
+                if(target == null){
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                            u.getLocale().getConfig().getString("players.unknown")).replace("<name>", args[1]));
+                    return true;
+                }
+                if(!sender.hasPermission("toolsies.kits.others.bypass")){
+                    if(!target.hasPermission("toolsies.kits." + kit) || !sender.hasPermission("toolsies.kits.others." + kit)){
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                                u.getLocale().getConfig().getString("kit.cant_gift_to_that_player")).replace("<name>", kit).replace("<player>", target.getName()));
+                        return true;
+                    }
+                }
+                kitManager.giveKit(target, kit);
+                if(sender == target){
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                            u.getLocale().getConfig().getString("kit.kit_applied")).replace("<name>", kit));
+                } else {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                            u.getLocale().getConfig().getString("kit.gifted_kit_to_player")).replace("<name>", kit).replace("<player>", target.getName()));
+                    User utarget = userManager.getUser(target);
+                    target.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                            utarget.getLocale().getConfig().getString("kit.player_gifted")).replace("<name>", kit).replace("<admin>", sender.getName()));
+                }
+                return true;
             }
+        }
+        Set<String> all = kitManager.getKits(sender);
+        if (all.isEmpty()) {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                    u.getLocale().getConfig().getString("kit.no_kits_available")));
+        } else {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                    u.getLocale().getConfig().getString("kit.available_kits")).replace("<kits>", all.toString()));
         }
         return true;
     }
