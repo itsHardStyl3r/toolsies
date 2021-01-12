3bb2dcf613538295cab293481ede03f0cb444335
Aleksander Gencel <johnnylittleplanet@gmail.com>
2021-01-12 15:53:57 +0100

    Kits: Support permissions and handle empty kit list
     * The kit command will now print only available kits.
       When user has no permission to get the kit, they'll
       be informed that kit doesn't exist.
       This avoids confusion when user asks for supported kit,
       but doesn't have permission to use it.
     * Avoid NullPointerException when kits section is not
       set or "kits:" is empty.
     * Tell user that there are no available kits when
       it's actually the case, too.
     * fixup: Fix copypaste when checking for permission.

--- src/main/java/me/hardstyl3r/toolsies/commands/kitCommand.java
+++ src/main/java/me/hardstyl3r/toolsies/commands/kitCommand.java
@@ -10,6 +10,8 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.Set;
+
 public class kitCommand implements CommandExecutor {
 
     private final KitManager kitManager;
@@ -23,25 +25,36 @@ public class kitCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if(!(sender instanceof Player)){
+        if (!(sender instanceof Player)) {
             sender.sendMessage("This command does not work in console.");
             return true;
         }
         User u = userManager.getUser(sender);
         if (!sender.hasPermission("toolsies.kit")) {
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', u.getLocale().getConfig().getString("no_permission")).replace("<permission>", "toolsies.locale"));
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                    u.getLocale().getConfig().getString("no_permission")).replace("<permission>", "toolsies.kit"));
             return true;
         }
         if (args.length == 1) {
-            if (!kitManager.isKit(args[0])) {
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', u.getLocale().getConfig().getString("kit.unknown_kit")).replace("<name>", args[0]));
+            String kit = args[0];
+            if (!kitManager.isKit(kit) || !sender.hasPermission("toolsies.kits." + kit.toLowerCase())) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                        u.getLocale().getConfig().getString("kit.unknown_kit")).replace("<name>", kit));
                 return true;
             }
             Player p = (Player) sender;
-            kitManager.giveKit(p, args[0]);
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', u.getLocale().getConfig().getString("kit.kit_applied")).replace("<name>", args[0]));
+            kitManager.giveKit(p, kit);
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                    u.getLocale().getConfig().getString("kit.kit_applied")).replace("<name>", kit));
         } else {
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', u.getLocale().getConfig().getString("kit.available_kits")).replace("<kits>", kitManager.getKits().toString()));
+            Set<String> all = kitManager.getKits(sender);
+            if (all.isEmpty()) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                        u.getLocale().getConfig().getString("kit.no_kits_available")));
+            } else {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                        u.getLocale().getConfig().getString("kit.available_kits")).replace("<kits>", all.toString()));
+            }
         }
         return true;
     }
--- src/main/java/me/hardstyl3r/toolsies/managers/KitManager.java
+++ src/main/java/me/hardstyl3r/toolsies/managers/KitManager.java
@@ -2,18 +2,20 @@ package me.hardstyl3r.toolsies.managers;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
 public class KitManager {
 
-    private FileConfiguration config;
+    private final FileConfiguration config;
 
     public KitManager(ConfigManager configManager) {
         config = configManager.loadConfig("kits");
@@ -24,29 +26,43 @@ public class KitManager {
     }
 
     public boolean isKit(String kit) {
-        return getConfig().get("kits." + kit.toLowerCase()) != null;
+        return getKits().contains(kit);
     }
 
     public Set<String> getKits() {
-        return getConfig().getConfigurationSection("kits").getKeys(false);
+        if (config.getConfigurationSection("kits") == null) {
+            return Collections.emptySet();
+        } else {
+            return config.getConfigurationSection("kits").getKeys(false);
+        }
+    }
+
+    public Set<String> getKits(CommandSender sender) {
+        Set<String> kits = getKits();
+        for (String s : getKits()) {
+            if (!sender.hasPermission("toolsies.kits." + s)) {
+                kits.remove(s);
+            }
+        }
+        return kits;
     }
 
     public void giveKit(Player p, String kit) {
         kit = kit.toLowerCase();
-        for (String kitMaterial : getConfig().getConfigurationSection("kits." + kit + ".items").getKeys(false)) {
+        for (String kitMaterial : config.getConfigurationSection("kits." + kit + ".items").getKeys(false)) {
             Material material = Material.matchMaterial((kitMaterial.contains("-") ? kitMaterial.split("-")[0] : kitMaterial));
             if (material == null) {
                 System.out.println("Kit " + kit + ": " + kitMaterial + " is not correct Material.");
                 return;
             }
-            String amount = getConfig().getString("kits." + kit + ".items." + kitMaterial + ".amount");
+            String amount = config.getString("kits." + kit + ".items." + kitMaterial + ".amount");
             if (Integer.valueOf(amount) == null || Integer.parseInt(amount) <= 0) {
                 System.out.println("Kit " + kit + ": Amount (" + amount + ") of " + kitMaterial + " is wrong.");
                 return;
             }
             ItemStack item = new ItemStack(material, Integer.parseInt(amount));
             ItemMeta im = item.getItemMeta();
-            List<String> lore = getConfig().getStringList("kits." + kit + ".items." + kitMaterial + ".lore");
+            List<String> lore = config.getStringList("kits." + kit + ".items." + kitMaterial + ".lore");
             if (!lore.isEmpty()) {
                 List<String> colorlore = new ArrayList<>();
                 for (String s : lore) {
@@ -55,7 +71,7 @@ public class KitManager {
                 }
                 im.setLore(colorlore);
             }
-            String name = getConfig().getString("kits." + kit + ".items." + kitMaterial + ".name");
+            String name = config.getString("kits." + kit + ".items." + kitMaterial + ".name");
             if (name != null) {
                 im.setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
             }
