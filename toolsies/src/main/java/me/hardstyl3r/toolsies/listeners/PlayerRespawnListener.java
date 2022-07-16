8319c2ca5c5d6554f067eea132fda97bfd36b628
Aleksander Gencel <johnnylittleplanet@gmail.com>
2022-07-16 12:22:25 +0200

    toolsies: listeners: Do not teleport Player to spawn if it's undefined
     * java.lang.NullPointerException: Cannot invoke "me.hardstyl3r.toolsies.objects.Spawn.isPreferred()" because "s" is null

--- toolsies/src/main/java/me/hardstyl3r/toolsies/listeners/PlayerRespawnListener.java
+++ toolsies/src/main/java/me/hardstyl3r/toolsies/listeners/PlayerRespawnListener.java
@@ -3,6 +3,7 @@ package me.hardstyl3r.toolsies.listeners;
 import me.hardstyl3r.toolsies.Toolsies;
 import me.hardstyl3r.toolsies.managers.LocationManager;
 import me.hardstyl3r.toolsies.objects.Spawn;
+import me.hardstyl3r.toolsies.utils.LogUtil;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -21,6 +22,10 @@ public class PlayerRespawnListener implements Listener {
     public void onRespawn(PlayerRespawnEvent e) {
         Player p = e.getPlayer();
         Spawn s = locationManager.getSpawn(p.getLocation());
+        if (s == null) {
+            LogUtil.warn("[toolsies] onRespawn(): Player could not have been teleported, spawn location is undefined.");
+            return;
+        }
         e.setRespawnLocation(s.isPreferred() ? s.getLocation() : locationManager.getDefaultSpawn().getLocation());
     }
 }
