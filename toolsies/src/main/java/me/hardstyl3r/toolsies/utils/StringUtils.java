20e4a58f5c61b9af7987fd10d73991790127a539
Aleksander Gencel <johnnylittleplanet@gmail.com>
2022-07-25 13:23:17 +0200

    toolsies: StringUtils: Create function to determine whether String is UUID

--- toolsies/src/main/java/me/hardstyl3r/toolsies/utils/StringUtils.java
+++ toolsies/src/main/java/me/hardstyl3r/toolsies/utils/StringUtils.java
@@ -1,6 +1,10 @@
 package me.hardstyl3r.toolsies.utils;
 
+import java.util.regex.Pattern;
+
 public class StringUtils {
+
+    private static final Pattern UUID_PATTERN = Pattern.compile("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$");
 
     /**
      * A function to determine whether a given string is a number.
@@ -14,5 +18,16 @@ public class StringUtils {
         for (int i = 0; i < s.length(); ++i)
             if (!Character.isDigit(s.charAt(i))) return false;
         return true;
+    }
+
+    /**
+     * A function to determine whether a given string is a correct UUID.
+     *
+     * @param s A string to be checked.
+     * @return True if is UUID, false if not.
+     */
+    public static boolean isUUID(String s) {
+        if (s == null || s.isBlank()) return false;
+        return UUID_PATTERN.matcher(s).matches();
     }
 }
