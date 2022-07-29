292b4fda6073813be274a57d74e39483a9e801cc
Aleksander Gencel <johnnylittleplanet@gmail.com>
2022-07-29 10:28:30 +0200

    toolsiesFamily: Bump required toolsies version to 0.12

--- tChat/src/main/java/me/hardstyl3r/tchat/TChat.java
+++ tChat/src/main/java/me/hardstyl3r/tchat/TChat.java
@@ -39,8 +39,8 @@ public class TChat extends JavaPlugin {
             if (toolsies == null || !toolsies.isEnabled())
                 throw new Exception("toolsies is null or not enabled");
             double version = Double.parseDouble(toolsies.getDescription().getVersion().split("-")[0]);
-            if (version < 0.10)
-                throw new Exception("unsupported toolsies version (<0.10)");
+            if (version < 0.12)
+                throw new Exception("unsupported toolsies version (<0.12)");
         } catch (Exception e) {
             LogUtil.error("[tChat] Could not hook into toolsies: " + e + ". Disabling.");
             this.setEnabled(false);
--- tPerms/src/main/java/me/hardstyl3r/tperms/TPerms.java
+++ tPerms/src/main/java/me/hardstyl3r/tperms/TPerms.java
@@ -34,8 +34,8 @@ public class TPerms extends JavaPlugin {
             if (toolsies == null || !toolsies.isEnabled())
                 throw new Exception("toolsies is null or not enabled");
             double version = Double.parseDouble(toolsies.getDescription().getVersion().split("-")[0]);
-            if (version < 0.10)
-                throw new Exception("unsupported toolsies version (<0.10)");
+            if (version < 0.12)
+                throw new Exception("unsupported toolsies version (<0.12)");
         } catch (Exception e) {
             LogUtil.error("[tPerms] Could not hook into toolsies: " + e + ". Disabling.");
             this.setEnabled(false);
