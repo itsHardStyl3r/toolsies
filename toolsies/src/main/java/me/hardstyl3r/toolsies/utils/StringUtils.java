552c35fca17fc2dcecf40aaafd64b54620beb4d1
Aleksander Gencel <johnnylittleplanet@gmail.com>
2022-12-16 14:04:54 +0100

    toolsies: Create legacy function for HEX ChatColors [1/2]

--- toolsies/src/main/java/me/hardstyl3r/toolsies/utils/StringUtils.java
+++ toolsies/src/main/java/me/hardstyl3r/toolsies/utils/StringUtils.java
@@ -1,10 +1,14 @@
 package me.hardstyl3r.toolsies.utils;
 
+import net.md_5.bungee.api.ChatColor;
+
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringUtils {
 
     private static final Pattern UUID_PATTERN = Pattern.compile("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$");
+    private static final Pattern HEX_COLORS = Pattern.compile("#[a-fA-F0-9]{6}");
 
     /**
      * A function to determine whether a given string is a number.
@@ -29,5 +33,20 @@ public class StringUtils {
     public static boolean isUUID(String s) {
         if (s == null || s.isBlank()) return false;
         return UUID_PATTERN.matcher(s).matches();
+    }
+
+    /**
+     * This method translates only legacy (&) and the new hex color codes.
+     * Legacy: &{color}; Hex: #{color}
+     */
+    public static String translateBothColorCodes(String message) {
+        Matcher matcher = HEX_COLORS.matcher(message);
+
+        while (matcher.find()) {
+            String color = message.substring(matcher.start(), matcher.end());
+            message = message.replace(color, ChatColor.of(color) + "");
+            matcher = HEX_COLORS.matcher(message);
+        }
+        return ChatColor.translateAlternateColorCodes('&', message);
     }
 }
